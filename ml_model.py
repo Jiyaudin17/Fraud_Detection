e90f567adc50bb28f12c0deacb074c09fb0f0cec
jiyaudin17 <jiyaudin94@live.com.sg>
2025-07-22 04:09:26 +0800

    adding muliple ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,16 @@
 print("Addding ml model code")
+
+
+
+def model1():
+    print('adding cod efor model1')
+
+
+
+def model3():
+    print('adding cod efor model2')
+
+
+def model3():
+    print('adding cod efor model3')
+
